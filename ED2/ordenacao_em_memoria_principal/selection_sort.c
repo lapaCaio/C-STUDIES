9a723f331e13ff62bf4c6774e1c96ed4a585a6c5
lapaCaio <lapacaio2001@gmail.com>
2022-10-04 15:03:39 -0300

    ADD - selection_sort.c
    adição do método de seleção direta

---
+++ ED2/ordenacao_em_memoria_principal/selection_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 9
+
+void selection_sort(int vet[], int n){
+    int menor, aux;
+
+    for(int i = 0; i < n - 1; i++){
+        menor = i;
+        for(int j = i + 1; j < n; j++){
+            if(vet[menor] > vet[j]){
+                menor = j; //posição do menor valor do vetor
+            }
+            if(i != menor){  //se a pos for diferente, troca 
+                aux = vet[i];
+                vet[i] = vet[menor];
+                vet[menor] = aux;
+            }
+            for(int a = 0; a < n; a++){
+                printf("%d, ", vet[a]);
+            }
+            printf("\n");
+        }
+    }
+}
+
+int main(int argv, char** argc){
+
+    int vet[MAX] = {3,6,5,1,2,8,7,9,4};
+
+
+    selection_sort(vet, MAX);
+
+    for(int i = 0; i < MAX; i++){
+        printf("%d - ", vet[i]);
+    }
+
+    return EXIT_SUCCESS;
+}
