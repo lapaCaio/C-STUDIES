f2d355343164fc93e34cdd4195ce0f48d95d8684
lapaCaio <lapacaio2001@gmail.com>
2022-10-24 16:27:58 -0300

    UPD - merge_sort.c

--- ED2/ordenacao_em_memoria_principal/merge_sort.c
+++ ED2/ordenacao_em_memoria_principal/merge_sort.c
@@ -1,60 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define MAX 9
 
-void merge(int *vet, int inicio, int meio, int fim){
-    int temp, p1, p2, tamanho, i, j, k;
+void merge(int *V, int inicio, int meio, int fim){
+    int *temp, p1, p2, tamanho, i, j, k;
     int fim1 = 0, fim2 = 0;
 
     tamanho = fim - inicio + 1;
     p1 = inicio;
     p2 = meio + 1;
+
     temp = (int *)malloc(tamanho * sizeof(int));
 
-    if(temp != NULL){
-        for(i = 0; i < tamanho; i++){
-            if(!fim1 && fim2){
-                if(vet[p1] < vet[p2]){
-                    temp[i] = vet[p1++];
+    if (temp != NULL){
+        for (i = 0; i < tamanho; i++){
+            if (!fim1 && !fim2){
+                if (V[p1] < V[p2]){
+                    temp[i] = V[p1++];
                 }else{
-                    temp[i] = vet[p2++];
+                    temp[i] = V[p2++];
                 }
-
-                if(p1 > meio){
+                if (p1 > meio){
                     fim1 = 1;
                 }
-                if(p2 > fim){
+                if (p2 > fim){
                     fim2 = 1;
                 }
             }else{
-                if(!fim1){
-                    temp[i] = vet[p1++];
+                if (!fim1){
+                    temp[i] = V[p1++];
                 }else{
-                    temp[i] = vet[p2++];
+                    temp[i] = V[p2++];
                 }
             }
         }
-        for(j = 0, k = inicio; j < tamanho; j++, k++){
-            vet[k] = temp[j];
-        }
+        for (j = 0, k = inicio; j < tamanho; j++, k++){
+            V[k] = temp[j];
+        }       
     }
     free(temp);
 }
 
-void merge_sort(int *vet[], int inicio, int fim){
+void merge_sort(int *V, int inicio, int fim){
     int meio;
-
-    if(inicio < fim){
+    if (inicio < fim){
         meio = floor((inicio + fim) / 2);
-        merge_sort(vet, inicio, meio);
-        merge_sort(vet, meio + 1, fim);
-        merge(vet, inicio, meio, fim);
+        merge_sort(V, inicio, meio);
+        merge_sort(V, meio + 1, fim);
+        merge(V, inicio, meio, fim);
     }
 }
 
-int main(int argv, char** argc){
+int main(int argv, char **argsc)
+{
 
+    int vet[MAX] = {3, 6, 5, 1, 2, 8, 7, 9, 4};
+
+    merge_sort(vet, 0, MAX - 1);
+
+    for (int i = 0; i < MAX; i++){
+        printf("%d - ", vet[i]);
+    }
 
     return EXIT_SUCCESS;
 }
