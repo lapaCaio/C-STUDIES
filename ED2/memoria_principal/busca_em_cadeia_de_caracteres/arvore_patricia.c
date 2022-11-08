d92983fb195440b7796a3b5187937f4b3cf998e2
lapaCaio <lapacaio2001@gmail.com>
2022-11-07 23:15:57 -0300

    ...

--- ED2/memoria_principal/busca_em_cadeia_de_caracteres/arvore_patricia.c
+++ ED2/memoria_principal/busca_em_cadeia_de_caracteres/arvore_patricia.c
@@ -4,108 +4,114 @@
 #define MAX 8
 
 // tipo_no = 0 -> FOLHA
-//tipo_no = 1 -> INTERMEDIÁRIO (TRATAR PRIMEIRO);
+// tipo_no = 1 -> INTERMEDIÁRIO (TRATAR PRIMEIRO);
 
-typedef struct no_pat{
+typedef struct no_pat
+{
     int tipo_no;
     int pos;
     char info[MAX];
     struct no_pat *esq;
     struct no_pat *dir;
-}NoPat;
+} NoPat;
 
-NoPat* criar_vazia(){
+NoPat *criar_vazia()
+{
     return NULL;
 }
 
-int esta_vazia(NoPat* np){
+int esta_vazia(NoPat *np)
+{
     return (np == NULL);
 }
 
-NoPat* insere(NoPat* np, char c[MAX]){
-    if(esta_vazia(np)){  //se arvore esta vazia
-        np = (NoPat*)malloc(sizeof(NoPat));
+NoPat *insere(NoPat *np, char c[MAX])
+{
+    if (esta_vazia(np))
+    { // se arvore esta vazia
+        np = (NoPat *)malloc(sizeof(NoPat));
         np->esq = np->dir = NULL;
         *(np->info) = c;
         np->tipo_no = 0;
         np->pos = MAX;
-    }else{  //FAZER FUNÇÃO DE BUSCA 
-        for(int i = 0; i < MAX; i++){
-            if(){
-
+    }
+    else
+    { // FAZER FUNÇÃO DE BUSCA
+        for (int i = 0; i < MAX; i++)
+        {
+            if ()
+            {
             }
         }
     }
 }
 
-NoPat* busca(NoPat* np, char c[MAX]){
-    if(esta_vazia(np)){
+NoPat *busca(NoPat *np, char c[MAX])
+{
+    if (esta_vazia(np))
+    {
         return NULL;
-    }else{
-        if(np->tipo_no != 0){
+    }
+    else
+    {
+        if (np->tipo_no != 0)
+        {
             busca(np->esq, c);
             busca(np->dir, c);
-        }else{
-            //criar um nó
+        }
+        else
+        {
+            
         }
     }
 }
 
-//REMOÇÃO ÁRVORE PATRÍCIA
-void remove_pat(NoPat** p, char c[MAX]){
-    int altera;
-    if(*p != NULL){
-        altera = remove_pat2(p, chave);
-        if(altera){
-            *p = NULL;
-        }  
-    }
-} //código provavlemente inútil... sla...
-
-int remove_pat2(NoPat** p, char[MAX]){
-    NoPat* temp;
+int remove_pat2(NoPat **p, char[MAX])
+{
+    NoPat *temp;
     int altera;
 
-    if((*p)->tipo_no == 0){
-            if(strcmp((*p)->c, c) == 0){
+    if ((*p)->tipo_no == 0)
+    {
+        if (strcmp((*p)->c, c) == 0)
+        {
             free(*p);
             return 1;
-        }else{
+        }
+        else
+        {
             return 0;
         }
-    }else{
-        if(c[(*p)->pos] == 0){
+    }
+    else
+    {
+        if (c[(*p)->pos] == 0)
+        {
             altera = remove_pat2(&((*p)->esq), c);
 
-            if(altera){
+            if (altera)
+            {
                 temp = *p;
                 *p = temp->dir;
                 free(temp);
             }
-        }else{
+        }
+        else
+        {
             altera = remove_pat2(&((*p)->dir), c);
-            if(altera){
+            if (altera)
+            {
                 temp = *p;
                 *p = (*p)->esq;
                 free(temp);
                 return 0;
             }
-        }   
-    }  
+        }
+    }
 }
 
-/*
-POSSIBILIDADES DE INSERÇÃO:
-1. ARVORE VAZIA
-2. NÓ INTERMEDIÁRIO... comparar os bits e posicionar no local certo. ARVORE CHEIA E INSERÇÃO EM UMA POSIÇÃO NO MEIO DA ARVORE.(NÓ INTERMEDIÁRIO);
-*/
-
-
-int main(int argv, char** argc){
-
-
-
-
+int main(int argv, char **argc)
+{
 
     return EXIT_SUCCESS;
 }
